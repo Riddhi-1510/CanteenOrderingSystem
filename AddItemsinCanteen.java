b7c0a7da39b55383d3ff0dfe97fc2eefc81c8d5a
Riddhi Mistry <202201238@daiict.ac.in>
2024-06-12 12:33:40 +0530

    add canteen operations

--- AddItemsinCanteen.java
+++ AddItemsinCanteen.java
@@ -3,14 +3,15 @@ import java.util.List;
 import java.util.Scanner;
 
 public class AddItemsinCanteen {
-    static List<EachItemData> items = new ArrayList<EachItemData>(2);
+    static List<EachItemData> items = new ArrayList<EachItemData>();
+    static int itemid = 1;
 
     static void addITEMS() {
         Scanner scan = new Scanner(System.in);
 
-        for (int index = 0; index < 2; index++) {
-            System.out.println("Enter id :");
-            int id = scan.nextInt();
+        for (int index = 0; index < 1; index++) {
+            int id = itemid;
+            itemid++;
 
             System.out.println("Enter item name : ");
             String name = scan.next();
@@ -29,7 +30,15 @@ public class AddItemsinCanteen {
 
             items.add(i);
         }
-        scan.close();
+    }
+
+    static void updatePrice(int id, int updatePrice) {
+        for (EachItemData item : items) {
+            if (item.getId() == id) {
+                item.setPrice(updatePrice);
+                break;
+            }
+        }
     }
 
     static void AllitemsInCanteenAvL() {
@@ -41,7 +50,7 @@ public class AddItemsinCanteen {
 
     static void OrderListUser(List<UserListData> l) {
         double totalAmount = 0;
-        for (UserListData item : l){
+        for (UserListData item : l) {
 
             totalAmount += (item.getQty() * item.getPrice());
 
@@ -54,39 +63,36 @@ public class AddItemsinCanteen {
 
     static void cancelOrderFunction(List<UserListData> l, int id, int q) {
         id--;
-        if(l.isEmpty()){
+        if (l.isEmpty()) {
             System.out.println("SORRY :<<<>>>:<<<<>>>:");
             return;
         }
         UserListData eid = (UserListData) l.get(id);// take bill id
-        if(eid.getQty() < q){
+        if (eid.getQty() < q) {// if by mistake use enter more then ordered qty
             System.out.println("SORRY ::::<<>>::::::");
             return;
         }
         int qty = eid.getQty();
         if (q == qty) {// remove
             l.remove(id);
-        } else if (qty - q < 0) {
-            System.out.println("ERROR NOT POSSIBLE SORRY :<");
         } else {
             // update the list
             ((UserListData) l.get(id)).setQty(qty - q);
 
             boolean flag = true;
-            if(!items.isEmpty()){
-                System.out.println("Enter ");
+            if (!items.isEmpty()) {
                 for (EachItemData itm : items) {
-                    if(itm.getId()==id+1){
+                    if (itm.getId() == id + 1) {
                         int qty1 = itm.getQty();
                         itm.setQty(qty1 + q);
-                        flag=false;
+                        flag = false;
                         break;
                     }
                 }
                 // System.out.println(items);
-            } 
+            }
 
-            if(flag==true){
+            if (flag == true) {// if item is finish then remove after added then new add available QTY
                 EachItemData eld = new EachItemData();
                 eld.setId(eid.getId());
                 eld.setItem_name(eid.getItem_name());
@@ -100,10 +106,10 @@ public class AddItemsinCanteen {
 
     static void takeOrderFunction(List<UserListData> l, int id, int q) {
         id--;
-        
+
         EachItemData eid = items.get(id);
 
-        if(eid.getQty()<=0 || eid.getQty() < q){
+        if (eid.getQty() <= 0 || eid.getQty() < q) {
             System.out.println("SORRY :<<<<<>>>>>:");
             return;
         }
@@ -117,21 +123,20 @@ public class AddItemsinCanteen {
             // System.out.println(items);
         }
         boolean flag = true;
-        if(!l.isEmpty()){
-            // System.out.println("Enter ");
+        if (!l.isEmpty()) {// update the user list
             for (UserListData itm : l) {
-                if(itm.getId()==id+1){
-                        
+                if (itm.getId() == id + 1) {
+
                     int qty = l.get(id).getQty();
                     l.get(id).setQty(q + qty);
                     flag = false;
                     break;
-                    
+
                 }
             }
             // System.out.println(items);
-        } 
-        if(flag==true){
+        }
+        if (flag == true) {// if user fresh order then create new food item
             UserListData uld = new UserListData();
             uld.setId(eid.getId());
             uld.setItem_name(eid.getItem_name());
@@ -140,8 +145,9 @@ public class AddItemsinCanteen {
 
             l.add(uld);
             l.get(id).setQty(q);// update QTY in User List
-            
+
         }
         // System.out.println(items);
     }
+
 }
--- SineStarCanteen.java
+++ SineStarCanteen.java
@@ -5,55 +5,111 @@ import java.util.Scanner;
 public class SineStarCanteen {
 
     public static void main(String[] args) {
-        // Operations.....Performed.........By........Canteen........In...........DAIICT  :><:
+
+        System.out.println("// -----------------------------------------------------//");
+        System.out.println("//                .....Operations.......               //");
+        System.out.println("//                ......Performed.......               //");
+        System.out.println("//                .........By...........               //");
+        System.out.println("//                .......Canteen........               //");
+        System.out.println("//                .........at...........               //");
+        System.out.println("//                .......DAIICT.........               //");
+        System.out.println("// -----------------------------------------------------//");
+
         Scanner scan = new Scanner(System.in);
 
-        List<UserListData> l = new ArrayList<UserListData>();//list for user
-
-        AddItemsinCanteen.addITEMS();//Add items first in canteen
+        List<UserListData> l = new ArrayList<UserListData>();// list for user
 
         while (true) {
-            System.out.println("Enter user ,  what they want : ");
-            System.out.println("1------List All items");
-            System.out.println("2------take order");
-            System.out.println("3------cancel order by user");
-            System.out.println("4------List item user order in current state");
-            System.out.println("5------DONE exit user");
-            int chs = scan.nextInt();
-            switch (chs) {
+            System.out.println("1 - Owner");
+            System.out.println("2 - Customer");
+            System.out.println("3 - ExitProgram");
+            System.out.println("Please select your role: ");
+            int ch = scan.nextInt();
+            if (ch == 3) {
+                System.out.println("Exit DAIICT canteen");
+                break;
+            }
+            switch (ch) {
                 case 1:
-                    AddItemsinCanteen.AllitemsInCanteenAvL();
+                    while (true) {
+                        System.out.println("1 - Add Items");
+                        System.out.println("2 - View All Items");
+                        System.out.println("3 - Update Item Price");
+                        System.out.println("4 - Exit Owner Menu");
+                        System.out.println("Please select an option: ");
+                        int choise = scan.nextInt();
+                        if (choise == 4) {
+                            System.out.println("Exiting the Owner menu...");
+                            break;
+                        }
+                        switch (choise) {
+                            case 1:
+                                AddItemsinCanteen.addITEMS();
+                                break;
+                            case 2:
+                                System.out.println("List of all available items in the canteen:");
+                                AddItemsinCanteen.AllitemsInCanteenAvL();
+                                break;
+                            case 3:// update the item price
+                                System.out.println("Enter item ID to update: ");
+                                int id = scan.nextInt();
+                                System.out.println("Enter new price: ");
+                                int price = scan.nextInt();
+                                AddItemsinCanteen.updatePrice(id, price);
+                                break;
+                            default:
+                                System.out.println("Invalid selection. Please try again.");
+                                break;
+                        }
+
+                    }
                     break;
                 case 2:
-                    System.out.println("Enter id : ");
-                    int id = scan.nextInt();
-                    System.out.println("Enter Qty : ");
-                    int qty = scan.nextInt();
-                    AddItemsinCanteen.takeOrderFunction(l,id,qty);
-                    break;
-                case 3:
-                    System.out.println("Enter id : ");
-                    int id1 = scan.nextInt();
-                    System.out.println("How many Qty remove in list : ");
-                    int qty1 = scan.nextInt();
-                    AddItemsinCanteen.cancelOrderFunction(l,id1,qty1);
-                    break;
-                case 4:
-                    AddItemsinCanteen.OrderListUser(l);
-                    break;
-                case 5:
-                    System.out.println("Exit this program");
-                    return;
+
+                    while (true) {
+                        System.out.println("1 - View All Items");
+                        System.out.println("2 - Place Order");
+                        System.out.println("3 - Cancel Order");
+                        System.out.println("4 - View Current Order");
+                        System.out.println("5 - Exit Customer Menu");
+                        System.out.println("Please select an option: ");
+                        int choise = scan.nextInt();
+                        if (choise == 5) {
+                            System.out.println("Exiting the customer menu...");
+                            break;
+                        }
+                        switch (choise) {
+                            case 1:
+                                AddItemsinCanteen.AllitemsInCanteenAvL();
+                                break;
+                            case 2:
+                                System.out.println("Enter item ID to order: ");
+                                int id = scan.nextInt();
+                                System.out.println("Enter quantity: ");
+                                int qty = scan.nextInt();
+                                AddItemsinCanteen.takeOrderFunction(l, id, qty);
+                                break;
+                            case 3:
+                                System.out.println("Enter item ID to cancel: ");
+                                int id1 = scan.nextInt();
+                                System.out.println("Enter quantity to remove: ");
+                                int qty1 = scan.nextInt();
+                                AddItemsinCanteen.cancelOrderFunction(l, id1, qty1);
+                                break;
+                            case 4:
+                                AddItemsinCanteen.OrderListUser(l);
+                                break;
+                            default:
+                                System.out.println("Invalid selection. Please try again.");
+                                break;
+                        }
+                    }
 
                 default:
-                    System.out.println("Invalid Number");
+                    System.out.println("Invalid selection. Please try again.");
                     break;
             }
         }
-        
+
     }
-    
-
-    
-
 }
